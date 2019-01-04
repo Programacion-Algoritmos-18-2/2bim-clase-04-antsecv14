cfdcf220eec6f1cda8713f56f007920f9d960312
Antse Caceres <antsecv14@gmail.com>
2019-01-04 16:25:11 -0500

    Entregado

--- trabajo_clase/modelado/archivo.py
+++ trabajo_clase/modelado/archivo.py
@@ -1,27 +1,27 @@
-
+#Importamos los codecs
 import codecs
-
+#Creamos la clase Equipo
 class Equipo(object):
-	def __init__(self,n,c,p,j):
-		self.nombres = n
+	def __init__(self,nom,c,p,j):
+		self.nombre = nom
 		self.ciudad = c
 		self.campeonatos = int(p)
-		self.numjugadores = j
-
-	def agregar_nombres(self,n):
-		self.nombres = n
+		self.numjugadores = int(j)
+#Creamos los get y set de cada variable
+	def agregar_nombre(self,nom):
+		self.nombre = nom
 
 	def agregar_ciudad(self,c):
-		self.apellidos = a
+		self.apellidos = c
 
 	def agregar_campeonatos(self,p):
-		self.edad = e
+		self.edad = int(p)
 
 	def agregar_numjugadores(self,j):
-		self.numjugadores = p
+		self.numjugadores = int(j)
 
-	def obtener_nombres(self):
-		return self.nombres
+	def obtener_nombre(self):
+		return self.nombre
 
 	def obtener_ciudad(self):
 		return self.ciudad
@@ -31,13 +31,13 @@ class Equipo(object):
 
 	def obtener_numjugadores(self):
 		return self.numjugadores
-
+#Creamos el str y el repr
 	def __str__(self):
-		return "%s %s %d %.2f" %(self.nombres, self.ciudad, self.campeonatos, self.numjugadores)
+		return "%s %s %d %.2f" %(self.nombre, self.ciudad, self.campeonatos, self.numjugadores)
 
 	def __repr__(self):
-		return "%s %s %d %.2f" %(self.nombres, self.ciudad, self.campeonatos, self.numjugadores)
-
+		return "%s %s %d %.2f" %(self.nombre, self.ciudad, self.campeonatos, self.numjugadores)
+#Creamos la clase MiArchivo
 class MiArchivo:
 
     def __init__(self):
@@ -49,29 +49,26 @@ class MiArchivo:
     def cerrar_archivo(self):
         self.archivo.close()
 
-
+#Creamos la clase Operaciones para ordenar la lista medienate los metodos ordenar y ordenar2
 class Operaciones(object):
     
     def __init__(self, listado):
         self.listado_equipo = listado
-
     def ordenar(self):
-        return sorted(self.listado_equipo, key=lambda equipo: equipo.nombres)
-
+        return sorted(self.listado_equipo, key=lambda equipo: equipo.obtener_nombre())
     def ordenar2(self):
-        return sorted(self.listado_equipo, key=lambda equipo: equipo.campeonatos)
+        return sorted(self.listado_equipo, key=lambda equipo: equipo.obtener_campeonatos())
 
 
-"""
 
+#Creamos MiArchivoEscribir para poder agregar toda la informacion ordenada en un nuevo archivo
 class MiArchivoEscribir:
 
     def __init__(self):
         self.archivo = codecs.open("data/ordenamiento.csv", "a")
 
     def agregar_informacion(self, p):
-        self.archivo.write(self.ordenar())
+        self.archivo.write("%s %s %d %d" %(p.nombre, p.ciudad, p.campeonatos, p.numjugadores))
 
     def cerrar_archivo(self):
         self.archivo.close()
-"""
--- trabajo_clase/principal.py
+++ trabajo_clase/principal.py
@@ -1,27 +1,42 @@
+#Importamos todas las clases del archivo archivo.py
 from modelado.archivo import *
 
+#Creamos los objetos de la clase MiArchivo y MiArchivoEscribit
 m = MiArchivo() 
 
-#m2 = MiArchivoEscribir() 
+m2 = MiArchivoEscribir() 
 
+#Creamos una lista para obtener la informacion del archivo mediante nuestro objeto
 lista = m.obtener_informacion()
 lista = [l.split("|") for l in lista]
-
+#Creamos lista2 para almacenar las listas ordenadas, y la lista_obj para almacenar los datos del archivo
+lista2 = []
 lista_obj = []
 
+#Mediante nuestro for agregamos la informacion a la lista_obj
 for d in lista:
 	p = Equipo(d[0],d[1],d[2],d[3])
+	lista_obj.append(p)
+#Creamos un objeto tipo operacion y le enviamos lista_obj
+operacion = Operaciones(lista_obj)
 
-operacion = Operaciones(lista)
+#Solicitamos al usuario que ingrese el metodo de ordenamiento que desee
+opc = int(input("Desea ordenar los equpos por : nombre(1) o por campeonato(2)\n"))
 
-print(operacion.ordenar())
+# dependiendo de la opcion la lista1 tomara el ordenamiento respectivo
+if opc == 1:
+	lista2 = operacion.ordenar()
 
-print(operacion.ordenar2())
+elif opc == 2:
+	lista2 = operacion.ordenar2()
 
-#ordenamiento = input("Desea ordenar por : nombre(1) \n\t\t\tcampeonato(2)\n")
+#Se agrega cada elemento de la lista 2 al metodo agregar informacion para escribir el archivo
+for d in lista2:
+	m2.agregar_informacion(d)
+print(lista2)
 
+print("--Proceso realizado con exito--")
 
-#if (ordenamiento == 1):
 
 	
 
